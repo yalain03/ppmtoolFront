15cda929f952a1e0559455b8eaa6e41e3cd04593
unknown <yalain9@gmail.com>
2019-07-10 16:32:59 -0500

    backlog actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,9 @@ export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_PROJECT = "GET_PROJECT";
 export const UPDATE_PROJECT = "UPDATE_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
+
+// Types for backlog actions
+
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
